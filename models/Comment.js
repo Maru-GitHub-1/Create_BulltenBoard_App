70a8ffb0fddd1dcc5730c686466f3a2a13507b7e
Maru <r51m12a1251@gmail.com>
2020-08-06 19:55:09 +0900

    Created update method on model

--- models/Comment.js
+++ models/Comment.js
@@ -1,3 +1,5 @@
+const { use } = require("../routers/comments");
+
 const comments = [];
 let nextId = 1;
 
@@ -43,4 +45,27 @@ module.exports = {
 
     return comment;
   },
+  update: ({id, username, body}) => {
+    if(typeof id !==  'number' || id < 1) {
+      throw new Error('idは必須です(１以上の数字)')
+    }
+    if (!username) {
+      throw new Error('usernameは必須です');
+    }
+    if (!body) {
+      throw new Error('bodyは必須です');
+    }
+
+    const comment = comments.find(comment => id === comment.id);
+
+    if (!comment) {
+      throw new Error('idに該当するコメントがありません');
+    }
+    comment.username = username;
+    comment.body = body;
+    comment.updatedAt = new Date();
+
+    return comment;
+  },
+
 }
