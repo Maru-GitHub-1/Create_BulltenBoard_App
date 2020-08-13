6b57549d59133d504c64fc5b5b99e937ba68c7a0
Maru <r51m12a1251@gmail.com>
2020-08-13 16:09:14 +0900

    Created CommentList Component in Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import Button from './Component/Button/Button'
+import Button from './Component/Button/Button';
+import CommentList from './Component/CommentList/CommentList'
 
 function App() {
+  const comments = [
+    'aaaaaa',
+    'bbbbbb',
+    'cccccc'
+  ];
+
   return (
     <div className="App">
       <Button>送信する</Button>
+      <CommentList comments={comments}></CommentList>
     </div>
   );
-}
+};
 
 export default App;
