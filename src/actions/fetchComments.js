3666eb0d374de841b2e662f142f9aa1a9d633130
Maru <r51m12a1251@gmail.com>
2020-08-19 16:37:07 +0900

    Created ActionCreator of GET and Created .babelrc

---
+++ src/actions/fetchComments.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const API_URL = '/api/comments';
+
+export const FETCH_COMMENTS_REQUEST = 'FETCH_COMMENTS_REQUEST';
+export const FETCH_COMMENTS_SUCCESS = 'FETCH_COMMENTS_SUCCESS';
+export const FETCH_COMMENTS_FAILURE = 'FETCH_COMMENTS_FAILURE';
+
+export const fetchComments = () => {
+  return async (dispatch) => {
+    dispatch( fetchCommentsRequest() );
+    try{
+      const response = await axios.get(API_URL);
+      const body = response.data.body;
+      const data = CommetnsModel.createCommentInstance(body);
+      dispatch( fetchCommentsSuccess(body) );
+    } catch (error) {
+      dispatch( fetchCommentsFailure(error) )
+    };
+  };
+};
+
+const fetchCommentsRequest = () => {
+  return {
+    type: FETCH_COMMENTS_REQUEST
+  }
+};
+
+const fetchCommentsSuccess = (data) => {
+  return {
+    type: FETCH_COMMENTS_SUCCESS,
+    data
+  }
+};
+
+const fetchCommentsFailure = (error) => {
+  return {
+    type: FETCH_COMMENTS_FAILURE,
+    error
+  }
+};
